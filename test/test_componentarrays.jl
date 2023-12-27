bc288b5df710e27c17672651e993d13fa5dae82c
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2023-12-27 11:07:58 +0100

    test ComponentArrays wrap_eltype

--- test/test_componentarrays.jl
+++ test/test_componentarrays.jl
@@ -4,7 +4,6 @@ using ComponentArrays
 
 @testset "ComponentVector" begin
     cv = ComponentVector(a = 1.0, b = [2.1, 2.2])
-    length(cv)
     cvw = @inferred wrap_size(cv)
     @test length(cvw) == length(cv)
     @test size(cvw) == size(cv)
@@ -20,4 +19,12 @@ end;
 @testset "error on non-matching dimensions" begin
     cv = ComponentVector(a = 1.0, b = [2.1, 2.2])
     @test_throws ErrorException SizeWrapper{7,(length(cv),),eltype(cv),ComponentArray}(cv)
-end
+end;
+
+@testset "wrap_eltye" begin
+    cv = ComponentVector(a = 1.0, b = [2.1, 2.2])
+    cvw = @inferred wrap_eltype(cv)
+    @test eltype(cvw) == eltype(cv)
+    @test wrapped_union(cvw) == ComponentArray
+    @test unwrap(cvw) == cv
+end;
